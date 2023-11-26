efa69a7a422a092f3017006a10d0d9be34c53c43
unknown <arunprasadh1996@gmail.com>
2023-11-26 15:47:32 +0530

    dependecies installed

--- solarathon/pages/__init__.py
+++ solarathon/pages/__init__.py
@@ -10,7 +10,7 @@ word_limit = solara.reactive(10)
 route_order = ["/", "settings", "chat", "clickbutton"]
 
 import os
-openai_api_key = os.getenv("OPENAI_API_KEY")
+#openai_api_key = os.getenv("OPENAI_API_KEY")
 
 @solara.component
 def Page():

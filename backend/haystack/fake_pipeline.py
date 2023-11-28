0e7d017e2c2e3cf2e2e7e8513829664a8a918296
simonefrisco <60827936+simonefrisco@users.noreply.github.com>
2023-11-28 14:57:40 +0000

    first dev init

---
+++ backend/haystack/fake_pipeline.py
@@ -0,0 +1,14 @@
+import os
+import json
+from pathlib import Path
+
+DISCORD_SERVER_ID = os.getenv('DISCORD_SERVER_ID')
+
+with open(f'data/{DISCORD_SERVER_ID}_selected_channels_messages.json', 'r') as f:
+    raw = json.loads(f.read())
+
+prod_data_path = Path('solarathon/assets/data')
+prod_data_path.mkdir(parents=True, exist_ok=True)
+
+with open(prod_data_path / f'haystack.json', 'w') as f:
+    json.dump(raw, f)

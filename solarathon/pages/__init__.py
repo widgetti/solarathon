45fa5d5332ae3a8f075b5c1b549edb0cf2bfdf82
simonefrisco <60827936+simonefrisco@users.noreply.github.com>
2023-11-30 16:18:47 +0000

    review fe 1

--- solarathon/pages/__init__.py
+++ solarathon/pages/__init__.py
@@ -20,33 +20,27 @@ DISCORD_SERVER_ID = os.getenv("DISCORD_SERVER_ID")
 @solara.component
 def Page():
     with solara.Column(style={"padding-top": "30px"}):
-        solara.Title("Solarathon example project")
-        # Calculate word_count within the component to ensure re-execution when reactive variables change.
-        word_count = len(sentence.value.split())
+        with solara.Column(align='center',gap='4px'):
+            solara.Markdown("# Welcome to", style={"padding":"0px 0px 0px 0px","font-size":"16px"})
+            solara.Markdown("## Solara Help Center", style={"padding":"0px 0px 0px 0px","font-size":"32px"})
 
-        solara.SliderInt("Word limit", value=word_limit, min=2, max=20)
-        solara.InputText(label="Your sentence", value=sentence, continuous_update=True)
-
-        # Display messages based on the current word count and word limit.
-        if word_count >= int(word_limit.value):
-            solara.Error(f"With {word_count} words, you passed the word limit of {word_limit.value}.")
-        elif word_count >= int(0.8 * word_limit.value):
-            solara.Warning(f"With {word_count} words, you are close to the word limit of {word_limit.value}.")
-        else:
-            solara.Success("Great short writing!")
-        
-        solara.Markdown(f"openai key {openaikey}")
-        solara.Markdown(f"discord server id {DISCORD_SERVER_ID}")
-        solara.Markdown("*First exercise*: remove this text and write your own sentence.")
-        solara.Markdown(f"""`Food`       
-        [Biriyani](https://en.wikipedia.org/wiki/Biryani)""")
-
+        with solara.Row(justify='center', style={'background-color':'rgb(28,43,51)', 'height':'250px'}):
+            with solara.Column(align='center', style={'background-color':'rgb(28,43,51)'}):
+                solara.Markdown('## Frequently Asked Questions', style={"padding":"12px 12px 12px 12px","font-size":"16px", "color":"white"})
+                with solara.Column(align='center', style={'background-color':'white', 'width':'320px'}):
+                    solara.InputText('Type a question . . .')
+                
         raw_data = Path(__file__).parent.parent
-        with open(raw_data / 'assets' / 'data' / 'haystack.json' , 'r' ) as f:
+        with open(raw_data / 'assets' / 'full_fe_faqs.json' , 'r' ) as f:
             raw = json.loads(f.read())
         solara.Markdown(f'{raw}')
 
 @solara.component
 def Layout(children):
     # this is the default layout, but you can override it here, for instance some extra padding
-    return solara.AppLayout(children=children, style={"padding": "20px"})
+    return solara.AppLayout(
+                    children=children, 
+                    navigation=False,
+                    style={"padding": "20px"}
+                    # style={"background-color": "red"}
+                    )

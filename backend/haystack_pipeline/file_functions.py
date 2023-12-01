cdbb12cb9d6b67ab68e0b5b84aaa9a465a1916ca
silvhua <silvhua@gmail.com>
2023-11-30 21:15:40 -0800

    parse retrieved docs as a list of dictionaries

--- backend/haystack_pipeline/file_functions.py
+++ backend/haystack_pipeline/file_functions.py
@@ -35,11 +35,41 @@ def haystack_doc_to_dict(input_string):
         return result_dict
     else:
         return None
+    
+def parse_input_string(input_string):
+    # Define a more flexible regular expression pattern
+    pattern = r'\n(?:Question\s*:\s*(.*?)\s*;)?(?:Answer\s*:\s*(.*?)\s*;)?(?:Integrations\s*:\s*(.*?)\s*;)?(?:Category\s*:\s*(.*?)\s*)?\n'
 
+    # Search for matches using the pattern
+    match = re.search(pattern, input_string)
+
+    # Check if a match is found
+    if match:
+        # Extract the captured groups (Question, Answer, Integrations, and Category)
+        question = match.group(1)
+        answer = match.group(2)
+        integrations = match.group(3)
+        category = match.group(4)
+
+        # Create and return a dictionary
+        result_dict = {'Question': question, 'Answer': answer, 'Integrations': integrations, 'Category': category}
+        return result_dict
+    else:
+        return None
 
 def list_retriever_results(retriever_results):
     """
-    
+    Function to convert Haystack results into a list of dictionaries containing 
+    the question and answer sets.
+
+    Parameters
+    ----------
+    retriever_results : Results from `retriever.run_query()`
+
+    Returns
+    -------
+    results : list
+        List of dictionaries containing the question and answer sets.
     """
     results = []
     for doc in retriever_results[0]['documents']:
--- backend/haystack_pipeline/step_3_retriever.py
+++ backend/haystack_pipeline/step_3_retriever.py
@@ -1,16 +1,23 @@
+from file_functions import *
+#%%
 import os
 from haystack.document_stores import FAISSDocumentStore
 from haystack.nodes import EmbeddingRetriever
+# import pdb ##### REMOVE IN FINAL SCRIPT
 
 ################
 #* THIS SCRIPT IS JUST A REFERENCE, WE COULD USE THE RETRIEVER FOR SEARCH FEATURE IN THE SOLARA FRONTEND
 ################
 
 DISCORD_SERVER_ID = os.getenv('DISCORD_SERVER_ID')
-DISCORD_MESSAGES_PATH_JSON = f'data/{DISCORD_SERVER_ID}_selected_channels_messages.json'
-DISCORD_MESSAGES_PATH_JSON_FORMATTED = f'data/filtered_{DISCORD_SERVER_ID}_selected_channels_messages.json'
-FULL_FAQS_PATH = 'data/full_faq.json'
-FAQS_PATH_JSON_FORMATTED = f'data/full_faq_formatted.json'
+data_path = 'data/'
+if os.getenv('run_locally') == '1':
+    data_path += '../../'
+
+DISCORD_MESSAGES_PATH_JSON = f'{data_path}{DISCORD_SERVER_ID}_selected_channels_messages.json'
+DISCORD_MESSAGES_PATH_JSON_FORMATTED = f'{data_path}filtered_{DISCORD_SERVER_ID}_selected_channels_messages.json'
+FULL_FAQS_PATH = f'{data_path}full_faq.json'
+FAQS_PATH_JSON_FORMATTED = f'{data_path}full_faq_formatted.json'
 
 embedding_model = 'sentence-transformers/all-mpnet-base-v2' # https://huggingface.co/sentence-transformers/all-mpnet-base-v2
 llm = 'gpt-3.5-turbo-16k'
@@ -19,19 +26,23 @@ index_filename = f'{DISCORD_SERVER_ID}_index.faiss'
 config_filename = f'{DISCORD_SERVER_ID}_config.json'
 faiss_filename = 'faiss_document_store.db'
 
+path = '../../solarathon/assets'
 document_store = FAISSDocumentStore.load( 
-    index_path  = f'solarathon/assets/{index_filename}', 
-    config_path = f'solarathon/assets/{config_filename}'
+    index_path  = f'{path}/{index_filename}', 
+    config_path = f'{path}/{config_filename}'
     )
 
 retriever = EmbeddingRetriever( # https://docs.haystack.deepset.ai/reference/retriever-api
     document_store=document_store,
     embedding_model=embedding_model,
     model_format="sentence_transformers",
-    top_k = 10
+    top_k = 5
 )
 
-print(
-    retriever.run_query('solara support fastapi?')
-)
+# ##### REMOVE IN FINAL SCRIPT
+# pdb.set_trace() 
+user_search_term = 'Jupyter notebook' #### MAKE THIS A UI INPUT
+retrieved_docs = retriever.run_query(user_search_term)
+results = list_retriever_results(retrieved_docs)
+
 
